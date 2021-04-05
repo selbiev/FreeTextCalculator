e1a37f19c6367a4a84ca2b32e90ef696d873dcbb
Said Elbiev <said.elbiev@gmail.com>
2021-04-05 10:29:02 +0200

    fixed add_analyze and mult_analyze methods

--- Analyzer.py
+++ Analyzer.py
@@ -36,15 +36,16 @@ def evaluate(n):
     return result
 
 def analyze_term(term):
+    term = preprocess(term)
     n = Node.Node()
     add_args = analyze_add(term)
-    mul_args = analyze_mult(term)
+    mult_args = analyze_mult(term)
     if len(add_args) > 1:
         n.op = "+"
         n.args = list(map(lambda t: analyze_term(t),add_args))
-    elif len(mul_args) > 1:
+    elif len(mult_args) > 1:
         n.op = "*"
-        n.args = list(map(lambda t: analyze_term(t),mul_args))
+        n.args = list(map(lambda t: analyze_term(t),mult_args))
     else:
         n.args = list(map(lambda t: float(t),add_args))
     return n
@@ -53,23 +54,23 @@ def analyze_add(l):
     l = str(l)
     args = []
     arg_until_now = ""
+    num_open_br = 0
+    num_closed_br = 0
+    """ the idea with the open/closed brackets is the following:
+        never split the word if you're inside a paranthesis term.
+        for example: 1 + 2 + (2 * 3 + 1) + 3
+        here, if you're inside the parentheses, ignore the + inside the parentheses
+        and dont split up the (2 * 3 + 1) in 2 pieces, but take it as whole into the
+        current add analysis. the analysis of the term inside the brackets will happen
+        at a lower level of the recursive algorithm """
 
     i = 0
     while i < (len(l)):
         if l[i] == "(":
-            num_open_brk = 1
-            arg_until_now += l[i]
-            i += 1
-            num_closed_brk = 0
-            while num_open_brk != num_closed_brk:
-                arg_until_now += l[i]
-                if l[i] == '(':
-                    num_open_brk += 1
-                if l[i] == ')':
-                    num_closed_brk += 1
-                i += 1
-            i -= 1
-        if l[i] == "+":
+            num_open_br += 1
+        if l[i] == ")":
+            num_closed_br += 1
+        if (l[i] == "+") and (num_open_br == num_closed_br):    #here, we avoid doing the error described above
             args.append(preprocess(arg_until_now))
             arg_until_now = ""
         else:
@@ -83,14 +84,17 @@ def analyze_mult(l):
     l = str(l)
     args = []
     arg_until_now = ""
+    num_open_br = 0
+    num_closed_br = 0
+    """ see comments in the add_analyze method """
 
     i = 0
     while i < (len(l)):
         if l[i] == "(":
-            while l[i] != ")":
-                arg_until_now += l[i]
-                i += 1
-        if l[i] == "*":
+            num_open_br += 1
+        if l[i] == ")":
+            num_closed_br += 1
+        if (l[i] == "*") and (num_open_br == num_closed_br):    #here, we avoid doing the error described above
             args.append(preprocess(arg_until_now))
             arg_until_now = ""
         else:
--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import Analyzer
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     term = "12 + 3 * 3 + (2 + (3 * 12 + 1) * 3) * 2 + 1"
+    term0 = "12 + (3 * (2 + (3 * 3 + 2))) * 3 + (2 + (3 * 12 + 1) * 3) * (2 * 3 * 1 + 1 + 2 * 3 + (9 * 9 + 22)) * 7 + 1"
     term1 = "(2 + (3 * 12))"
     term2 = "3 * (3 + 1) * 2"
     term3 = "2 * 3 + 3 * 3"
-    node = Analyzer.analyze_term(term1)
+    node = Analyzer.analyze_term(term0)
     print(Analyzer.evaluate(node))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
