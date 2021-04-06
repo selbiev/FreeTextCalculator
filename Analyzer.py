d50cb7e6031f418977ad6d29fbb3efd8f02bdf12
Said Elbiev <said.elbiev@gmail.com>
2021-04-06 19:14:39 +0200

    Added Subtraction and Division features

--- Analyzer.py
+++ Analyzer.py
@@ -3,8 +3,10 @@ import Node
 mapping = dict()        #the state of the program, e.g. for variable bindings or saved functions
 
 def preprocess(term : str) -> str:
+    if len(term) == 0:
+        return term
     if term[len(term)-1] == " ":        #check for trailing whitespace
-        term = term[:len(term)-1]       #delete trainling whitespace
+        term = term[:len(term)-1]       #delete trailing whitespace
         term = preprocess(term)         #recursively continue
     if term[0] == " ":                  #check for starting whitespace
         term = term[1:]                 #etc.
@@ -15,10 +17,20 @@ def preprocess(term : str) -> str:
     return term
 
 def analyze_term(term):
-    term = preprocess(term)
     n = Node.Node()
+    term = preprocess(term)
+
+    if term[0] == "-":      # was the term negated?
+        term = term[1:]
+        n.neg = True
+    elif term[0] == ":":    # was there a division?
+        term = term[1:]
+        n.inv = True
+
+    term = preprocess(term)
     add_args = analyze_add(term)
     mult_args = analyze_mult(term)
+
     if len(add_args) > 1:
         n.op = "+"
         n.args = list(map(lambda t: analyze_term(t),add_args))
@@ -44,18 +56,32 @@ def analyze_add(l):
         at a lower level of the recursive algorithm """
 
     i = 0
+    next_is_negative = False
     while i < (len(l)):
         if l[i] == "(":
             num_open_br += 1
-        if l[i] == ")":
+            arg_until_now += l[i]
+        elif l[i] == ")":
             num_closed_br += 1
-        if (l[i] == "+") and (num_open_br == num_closed_br):    #here, we avoid doing the error described above
+            arg_until_now += l[i]
+        elif (l[i] == "-") and (num_open_br == num_closed_br):    #here, we avoid doing the error described above
+            if next_is_negative:
+                arg_until_now = "-(" + preprocess(arg_until_now) + ")"
+            next_is_negative = True
+            args.append(preprocess(arg_until_now))
+            arg_until_now = ""
+        elif (l[i] == "+") and (num_open_br == num_closed_br):
+            if next_is_negative:
+                arg_until_now = "-(" + preprocess(arg_until_now) + ")"
+                next_is_negative = False
             args.append(preprocess(arg_until_now))
             arg_until_now = ""
         else:
             arg_until_now += l[i]
         i += 1
     if len(arg_until_now) > 0:
+        if next_is_negative:
+            arg_until_now = "-(" + preprocess(arg_until_now) + ")"
         args.append(preprocess(arg_until_now))
     return args
 
@@ -67,20 +93,33 @@ def analyze_mult(l):
     arg_until_now = ""
     num_open_br = 0
     num_closed_br = 0
-    """ see comments in the add_analyze method """
 
     i = 0
+    next_is_inverse = False
     while i < (len(l)):
         if l[i] == "(":
             num_open_br += 1
-        if l[i] == ")":
+            arg_until_now += l[i]
+        elif l[i] == ")":
             num_closed_br += 1
-        if (l[i] == "*") and (num_open_br == num_closed_br):    #here, we avoid doing the error described above
+            arg_until_now += l[i]
+        elif (l[i] == ":") and (num_open_br == num_closed_br):    #here, we avoid doing the error described above
+            if next_is_inverse:
+                arg_until_now = ":(" + preprocess(arg_until_now) + ")"
+            next_is_inverse = True
+            args.append(preprocess(arg_until_now))
+            arg_until_now = ""
+        elif (l[i] == "*") and (num_open_br == num_closed_br):
+            if next_is_inverse:
+                arg_until_now = ":(" + preprocess(arg_until_now) + ")"
+                next_is_inverse = False
             args.append(preprocess(arg_until_now))
             arg_until_now = ""
         else:
             arg_until_now += l[i]
         i += 1
     if len(arg_until_now) > 0:
+        if next_is_inverse:
+            arg_until_now = ":(" + preprocess(arg_until_now) + ")"
         args.append(preprocess(arg_until_now))
     return args
