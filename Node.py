6557bf18860de11c1b63e1220e49c49a77bcd49f
Said Elbiev <said.elbiev@gmail.com>
2021-04-06 19:15:03 +0200

    added inv flag to indicate that an expression is 1/expression

--- Node.py
+++ Node.py
@@ -1,6 +1,7 @@
 class Node:
     op = ""
-    neg = False
+    neg = False     # is the expression negated? done if input term was subtracted
+    inv = False     # is the expression inversed? done if input term was divided
     args = []
     function = False
     function_var = ""
