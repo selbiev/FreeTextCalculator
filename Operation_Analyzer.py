499e884c0d0fe630a22ae1ea98e8c9c701b6d543
Said Elbiev <said.elbiev@gmail.com>
2022-05-29 21:21:16 +0200

    Refactoring Operation Analyzer

--- Operation_Analyzer.py
+++ Operation_Analyzer.py
@@ -22,11 +22,12 @@ def init_fields(operation_, term_):
     operation = operation_
     term = str(term_)
     subterms = []
-    current_ptr = None
+    current_ptr = 0
     current_subterm = ""
     num_open_brackets = 0
     num_closed_brackets = 0
     inverse_operation = inverse_operations[operation]
+    following_term_is_inverse = False
 
 
 def analyze(operation_, term_):
@@ -41,13 +42,13 @@ def do_open_bracket():
 
 
 def do_close_bracket():
-    global num_closed_brackets, current_subterm, current_ptr
+    global num_closed_brackets, current_subterm
     num_closed_brackets += 1
     current_subterm += term[current_ptr]
 
 
-def add_subterm_if_outside_brackets_case_inverse():
-    global current_subterm, inverse_operation, following_term_is_inverse
+def add_subterm_and_set_next_term_as_inverse():
+    global current_subterm, following_term_is_inverse
     # we add subterms to the term list only if we're outside of the brackets
     if following_term_is_inverse:
         current_subterm = inverse_operation + "(" + str_utils.trim_brackets(current_subterm) + ")"
@@ -56,8 +57,8 @@ def add_subterm_if_outside_brackets_case_inverse():
     current_subterm = ""
 
 
-def add_subterm_if_outside_brackets():
-    global current_subterm, inverse_operation, following_term_is_inverse
+def add_subterm_normal_case():
+    global current_subterm, following_term_is_inverse
     if following_term_is_inverse:
         current_subterm = inverse_operation + "(" + str_utils.trim_brackets(current_subterm) + ")"
         following_term_is_inverse = False
@@ -65,25 +66,53 @@ def add_subterm_if_outside_brackets():
     current_subterm = ""
 
 
+def opening_bracket_next():
+    return term[current_ptr] == "("
+
+
+def closing_bracket_next():
+    return term[current_ptr] == ")"
+
+
+def inverse_operation_char_next():
+    return term[current_ptr] == inverse_operation
+
+
+def operation_char_next():
+    return term[current_ptr] == operation
+
+
+def no_unclosed_brackets():
+    return num_open_brackets == num_closed_brackets
+
+
+def add_current_char_to_subterm():
+    global current_subterm
+    current_subterm += term[current_ptr]
+
+
+def current_subterm_not_empty():
+    return len(current_subterm) > 0
+
+
 def analyze_operation():
-    global term, subterms, current_ptr, current_subterm, num_open_brackets, num_closed_brackets, \
-        inverse_operation, following_term_is_inverse
+    global current_ptr, current_subterm, following_term_is_inverse
 
     current_ptr = 0
     following_term_is_inverse = False
     while current_ptr < (len(term)):
-        if term[current_ptr] == "(":
+        if opening_bracket_next():
             do_open_bracket()
-        elif term[current_ptr] == ")":
+        elif closing_bracket_next():
             do_close_bracket()
-        elif (term[current_ptr] == inverse_operation) and (num_open_brackets == num_closed_brackets):
-            add_subterm_if_outside_brackets_case_inverse()
-        elif (term[current_ptr] == operation) and (num_open_brackets == num_closed_brackets):
-            add_subterm_if_outside_brackets()
+        elif inverse_operation_char_next() and no_unclosed_brackets():
+            add_subterm_and_set_next_term_as_inverse()
+        elif operation_char_next() and no_unclosed_brackets():
+            add_subterm_normal_case()
         else:
-            current_subterm += term[current_ptr]
+            add_current_char_to_subterm()
         current_ptr += 1
-    if len(current_subterm) > 0:
+    if current_subterm_not_empty():
         if following_term_is_inverse:
             current_subterm = inverse_operation + "(" + str_utils.trim_brackets(current_subterm) + ")"
         subterms.append(str_utils.trim_brackets(current_subterm))
